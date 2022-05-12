95a8b4aaf7554350ddc9067c32d75a0d289dc731
Rodrigo Castro <mrcastro9574@gmail.com>
2022-05-12 13:59:32 +0200

    Added google authentication

--- server/index.js
+++ server/index.js
@@ -1,23 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const passport = require('passport');
+const url = require('url');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// Get secret and client ids
+const keys = require('./config/keys');
 
-// Node environment on deploy:
-// engine property with specific version of node
+const app = express();
 
-// Dynamic port binding: Get port from runtime configuration 
-const PORT = process.env.PORT || 5000
+// Use passport with Google instance
+// Constructor requires StrategyOptions and callback with token, profile
+// that should execute when authentication is finished
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (token, _, profile) => {
+      console.log("data:")
+      console.log(profile);
+    }
+  )
+);
 
-// app: express App to register route
-// get/post: requests with this methids
-// path: requests accesing path
-// req: incoming request
-// res: outgoing response
-// () =>: callback
-app.get('/', (req, res) => {
-    console.log(req)
-  res.send({hello : 'world'})
-})
 
-app.listen(PORT, () => { 
-  console.log(`Example app listening on port ${PORT}`)
-})
+// Authenticate using google with access to this scope
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+}));
+// Handled Authorized redirect URIs from Google and authenticate again
+// then callback from GoogleStrategy will be executed
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
